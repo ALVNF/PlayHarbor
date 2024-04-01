2799ae578b31f4aa6dfa8e5417a39efa7ec43620
Blaya <120740708+Papapue@users.noreply.github.com>
2024-04-01 11:07:56 +0200

    Add files via upload

--- backend/mi-proyecto-backend/api/webSocket.js
+++ backend/mi-proyecto-backend/api/webSocket.js
@@ -1,34 +1,82 @@
 const express = require('express');
-var app = express();
-var datos;
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
-const WebSocket = require('ws')
+const router = express.Router();
+const WebSocket = require('ws');
+
+var datos2;
+
+const wsc = new WebSocket.Server({port:8082},()=>{
+    console.log('server started')
+})
 
 const wss = new WebSocket.Server({port:8081},()=>{
     console.log('server started')
 })
 
-wss.on('connection',(ws)=>{
-    ws.on('message',(data)=>{
-        console.log('data received %o '+data)
-        ws.send(data)
-        datos = data;
+wss.on('listening',()=>{
+    console.log('game is listening on port 8081');
+})
+
+wss.on('connection', (ws) => {
+    console.log('Juego conectado al servidor WebSocket');
+
+    // Manejar los mensajes recibidos desde el cliente
+    ws.on('message', (mensaje) => {
+        console.log('Mensaje recibido del juego: ' + mensaje);
+
+        try {
+            // Intentar analizar el mensaje JSON
+            const datos = JSON.parse(mensaje);
+            
+            console.log('Datos recibidos: ', datos);
+
+            datos2 = datos.puntuacionJ1
+            console.log(datos2);
+            
+            wsc.clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN) {
+                  client.send(JSON.stringify(datos));
+                }
+                });
+            
+            // Enviar el mensaje JSON como tengo en la imagen del bloc de notas de Pepe al cliente
+
+        } catch (error) {
+            console.error('Error al analizar el mensaje JSON:', error);
+        }
+    })
+
+    // Manejar la desconexión del cliente
+    ws.on('close', () => {
+        console.log('Juego desconectado del servidor WebSocket');
     })
 })
 
-wss.on('listening',()=>{
-    console.log('server is listening on port 8080');
+wsc.on('connection', (ws) => {
+    console.log('Cliente conectado al servidor WebSocket');
+
+    // Manejar los mensajes recibidos desde el cliente
+    ws.on('message', (mensaje) => {
+        console.log('Mensaje recibido del cliente: ' + mensaje);
+
+        try {
+            // Intentar analizar el mensaje JSON
+            const datos = JSON.parse(mensaje);
+            console.log('Datos recibidos: ', datos);
+
+            datos2 = datos.puntuacionJ1
+            console.log(datos2);
+
+            ws.send(JSON.stringify(datos));
+
+        } catch (error) {
+            console.error('Error al analizar el mensaje JSON:', error);
+        }
+    })
+
+    // Manejar la desconexión del cliente
+    ws.on('close', () => {
+        console.log('Cliente desconectado del servidor WebSocket');
+    })
 })
 
-app.get('/', function(req, res){
-    res.setHeader('Content-Type', 'text/plain');
-    res.status(200).send(datos);
-});
-
-server.listen(8080, function(){
-    console.log("Servidor corriendo en http://localhost:8080");
-});
-
-
-
+module.exports = router;
