2582b1637144c5bc8db24856fa12ad61ac5c146d
Blaya <120740708+Papapue@users.noreply.github.com>
2024-04-10 10:16:30 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -13,16 +13,32 @@ app.use('/juegos', express.static('backend/juegos/revisados'));
 const usuariosRutas = require('./backend/mi-proyecto-backend/api/usuarios');
 const authRutas = require('./backend/mi-proyecto-backend/api/auth');
 const juegosRutas = require('./backend/mi-proyecto-backend/api/juegos');
+const juegosLogros= require('./backend/mi-proyecto-backend/api/logros');
+const webJuegosRutas = require('./backend/mi-proyecto-backend/api/webSocket');
+//const mensajesRutas = require('./backend/mi-proyecto-backend/api/mensajes');
+
 app.use('/api', usuariosRutas);
 app.use('/api', authRutas);
 app.use('/api', juegosRutas);
+app.use('/api', juegosLogros);
+app.use('/api', webJuegosRutas);
+//app.use('/api', mensajesRutas);
+
 // Cargar clave privada y certificado para HTTPS
 const privateKey = fs.readFileSync(path.join(__dirname, 'server.key'), 'utf8');
 const certificate = fs.readFileSync(path.join(__dirname, 'server.cert'), 'utf8');
 
+//Quitar cache 
+app.use(function(req, res, next) {
+  res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.header('Pragma', 'no-cache');
+  res.header('Expires', '0');
+  next();
+});
+
 app.get('/', (req, res) => {
-  //res.sendFile(__dirname + '/frontend/index.html');
-  res.sendFile(__dirname + '/frontend/pages/descriptionGame.html');
+  res.sendFile(__dirname + '/frontend/pages/potato.html');
+  //res.sendFile(__dirname + '/frontend/pages/descriptionGame.html');
 });
 
 // app.get('/register', (req, res) => {
